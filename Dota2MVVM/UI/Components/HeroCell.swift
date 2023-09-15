d14768cd2b2f493c40141a7785856167f6d553d0
Andres Calizaya <andres.calizaya@naranjax.com>
2023-09-15 12:40:49 -0300

    Added Hero cell

---
+++ Dota2MVVM/UI/Components/HeroCell.swift
@@ -0,0 +1,20 @@
+//
+//  HeroCell.swift
+//  Dota2MVVM
+//
+//  Created by Andres Calizaya on 15/09/2023.
+//
+
+import SwiftUI
+
+struct HeroCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HeroCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HeroCell()
+    }
+}
