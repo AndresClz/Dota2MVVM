f8a5f5d1f0d9362b0aec55ef3b3f017ac37f2067
Andres Calizaya <andres.calizaya@naranjax.com>
2023-09-14 22:21:54 -0300

    Initial Commit

---
+++ Dota2MVVM/Dota2MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2MVVMApp.swift
+//  Dota2MVVM
+//
+//  Created by Andres Calizaya on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dota2MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
