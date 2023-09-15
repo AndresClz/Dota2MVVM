12262a75baa451cb5f47d94ad2df5ed4eabeae9d
Andres Calizaya <andres.calizaya@naranjax.com>
2023-09-15 09:51:58 -0300

    Add pull to refresh

--- Dota2MVVM/UI/Screens/Home/HomeView.swift
+++ Dota2MVVM/UI/Screens/Home/HomeView.swift
@@ -34,6 +34,9 @@ struct HomeView: View {
             // Cuando aparece la vista, carga los datos
             viewModel.loadData()
         }
+        .refreshable {
+            viewModel.loadData()
+        }
     }
 }
 
